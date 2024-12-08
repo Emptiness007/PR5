de8b26cb9fd5d0953d1a4d1e1aed21814f2eadfe
pokapachka <pachkaosokin@yandex.ru>
2024-12-08 23:09:27 +0500

    Прописан метод OnSetings. Осокин

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace Client
 {
     internal class Program
     {
+        static IPAddress ServerIpAddress;
+        static int ServerPort;
+        static string ServerToken;
+        static DateTime ClientDateConnection;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OnSetings();
+        }
+        static void OnSetings()
+        {
+            string Path = Directory.GetCurrentDirectory() + "/.config";
+            string IpAddress = "";
+            if (File.Exists(Path))
+            {
+                StreamReader streamReader = new StreamReader(Path);
+                IpAddress = streamReader.ReadLine();
+                ServerIpAddress = IPAddress.Parse(IpAddress);
+                ServerPort = int.Parse(streamReader.ReadLine());
+                streamReader.Close();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Server address: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(IpAddress);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Server port: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(ServerPort.ToString());
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Please provide the IP address if the license server: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                IpAddress = Console.ReadLine();
+                ServerIpAddress = IPAddress.Parse(IpAddress);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Please specify the license server port: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                ServerPort = int.Parse(Console.ReadLine());
+
+                StreamWriter streamWriter = new StreamWriter(Path);
+                streamWriter.WriteLine(IpAddress);
+                streamWriter.WriteLine(ServerPort.ToString());
+                streamWriter.Close();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("To change, write the command: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("/config");
         }
     }
 }
